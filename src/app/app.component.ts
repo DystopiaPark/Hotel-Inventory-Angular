9238038ada78899ffe6a3a138c37e97347d01ff4
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2023-04-21 14:20:39 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // html tag, "view"
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // template: `<h1>Hello World from inline template!</h1>
+  //   <p>Angular is Awesome</p> `,
+  styleUrls: ['./app.component.scss'],
+  styles: [
+    `
+      h1 {
+        color: red;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = 'hotelinventoryapp';
