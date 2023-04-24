7b5ec39692c9962939c17117528c9cf1d35ba6bd
Filip Stojanovic <mercurial.the.spectre@gmail.com>
2023-04-24 14:11:23 +0200

    update

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -67,4 +67,19 @@ export class RoomsComponent implements OnInit {
   selectRoom(room: RoomList) {
     this.selectedRoom = room;
   }
+
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 4,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner, Free Wi-Fi, TV, Bathroom, Kitchen',
+      price: 500,
+      photos: 'https://images.unsplash.com/photo-1518791831217-8f162f1e1131',
+      checkinTime: new Date('11-Nov-2021'),
+      checkoutTime: new Date('12-Nov-2021'),
+      rating: 4.5,
+    };
+
+    this.roomList.push(room);
+  }
 }
